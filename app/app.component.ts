fe3fa876797b44e845eaa3b8f3b1e00db737bc2d
James Osborn and Jessie Waite <james+jessie@>
2017-12-06 13:42:54 -0800

    add newKeg method, form for new Kegs.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,21 +5,61 @@ import { Component } from '@angular/core';
   template: `
   <h1>Tap Room</h1>
     <ul>
-    <li *ngFor="let currentBeer of beers">{{currentBeer.name}}</li>
+    <li (click)="viewBeer(currentBeer)" *ngFor="let currentBeer of beers">{{currentBeer.name}}</li>
     </ul>
+
+
+    <form>
+      <label for="beerName">Enter a beer name</label>
+      <input type="text" #beerName>
+      <label for="beerBrand">Enter the beer brand name</label>
+      <input type="text" #beerBrand>
+      <label for="beerPrice">Enter the beer price</label>
+      <input type="text" #beerPrice>
+      <label for="beerAlcoholContent">Enter the beer alcohol-content</label>
+      <input type="text" #beerAlcoholContent>
+      <button (click)="newKeg(beerName.value, beerBrand.value, beerPrice.value, beerAlcoholContent.value)">Add!</button>
+    </form>
+
+    <div class="beer-wrapper" *ngIf="selectedBeer">
+      <h2>{{selectedBeer.name}}<button (click)="editKeg()">Edit Keg</button></h2>
+      <h4>{{selectedBeer.brand}}<button (click)="editKeg()">Edit Keg</button></h4>
+      <p>\${{selectedBeer.price}} per pint<button (click)="editKeg()">Edit Keg</button></p>
+      <p>{{selectedBeer.alcoholContent}}%<button (click)="editKeg()">Edit Keg</button></p>
+      <p>{{selectedBeer.pintCount}}<button (click)="editKeg()">Edit Keg</button></p>
+    </div>
   `
 })
 
 export class AppComponent {
   beers: Beer[] = [
-    new Beer("Cascadia IPA", "Cascadia Brewing", "$5/pint", 6),
-    new Beer("Helles Belles Lager", "Ninkasi", "$6/pint", 5.3),
-    new Beer("Total Domination", "Ninkasi", "$7/pint", 6.7),
-    new Beer("Tricerahops", "Ninkasi", "5/pint", 8)
-  ]
+    new Beer("Cascadia IPA", "Cascadia Brewing", 5, 6),
+    new Beer("Helles Belles Lager", "Ninkasi", 6, 5.3),
+    new Beer("Total Domination", "Ninkasi", 7, 6.7),
+    new Beer("Tricerahops", "Ninkasi", 5, 8)
+  ];
+
+  editKeg() {
+    alert("You've requested to edit a beer!");
+  }
+
+  newKeg(name, brand, price, alcoholContent) {
+    let newBeer = new Beer(name, brand, price, alcoholContent);
+    this.beers.push(newBeer);
+  }
+
+  viewBeer(clickedBeer){
+    this.selectedBeer = clickedBeer;
+  }
+
+  hideBeer(){
+    this.selectedBeer = null;
+  }
+  selectedBeer = null;
+
 }
 
 export class Beer {
   public pintCount: number = 150;
-  constructor(public name: string, public brand: string, public price: string, public alcoholContent: number) {}
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: number) {}
 }
